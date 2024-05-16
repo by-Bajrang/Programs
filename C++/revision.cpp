c2bd618e81d54561913af4e4daa18d674fa30258
by-Bajrang <bybajrangyadav@gmail.com>
2024-05-16 20:49:21 +0530

    addup

---
+++ C++/revision.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+   // string s[10];
+   // s[0]='b';
+    //string s="bajrang yadav";
+   // cin>>s;
+   // cout<<s[0];
+  getline(cin,s);
+  cout<<s;
+    
+    return 0; 
+}
---
+++ pattern/practicr.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{ char ch='A';
+  int n = 5;
+  for (int i = 0; i < n; i++)
+  {
+    //space
+   for(int j=0;j<(n-1-i);j++)
+   {
+    cout<<" ";
+   }
+   // alphabate
+   for()
+    cout << endl;
+  }
+
+  return 0;
+}
