2210227c89ee5d229fda11c67c0c0e5ba200431b
by-Bajrang <bybajrangyadav@gmail.com>
2024-05-23 20:18:54 +0530

    added up

---
+++ practice/1st.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{  
+    int n=5; 
+    for (int i = 0; i < n; i++)
+    {
+      
+       cout<<endl;
+    }
+    return 0;
+}
---
+++ webdevelopment/25th/kk.py
@@ -0,0 +1,8 @@
+
+
+print("sdfghjkl")
+
+lst=[1,3,5,6,7]
+
+lst.sort()
+print(lst)
